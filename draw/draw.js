8ce4651369ed21f01a63086104b7a8644578e9e1
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-05-02 16:42:24 -0400

    Update notes for attendee draw (cvs to json)

--- draw/draw.js
+++ draw/draw.js
@@ -1,4 +1,9 @@
 'use strict';
+// --change the event id for the current meetup
+// from Meetup page: Tools/Download attendee liste.g.:
+// May 13 http://www.meetup.com/Ottawa-JavaScript/events/218835551/csv/
+// Jun 10 http://www.meetup.com/Ottawa-JavaScript/events/218835536/csv/
+// then convert tp json
 //    <!-- http://www.convertcsv.com/csv-to-json.htm -->
 
 
@@ -8,7 +13,7 @@ angular.module("draw", [])
         $scope.helloTo.title = "World, AngularJS";
         $scope.attendees = [{Name:'Daneroo'}];
 
-        $http.get('attendees-2015-03-11.json').then(function(resp) {
+        $http.get('attendees-2015-05-13.json').then(function(resp) {
             console.log(resp);
             $scope.attendees = resp.data;
             // console.log($scope.attendees);
