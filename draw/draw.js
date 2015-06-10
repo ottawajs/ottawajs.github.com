8a8d4e8b18519004fa86a6cfec538929687be4b2
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-06-09 20:32:46 -0400

    Update attendees: 2015-06-10

--- draw/draw.js
+++ draw/draw.js
@@ -3,6 +3,7 @@
 // from Meetup page: Tools/Download attendee liste.g.:
 // May 13 http://www.meetup.com/Ottawa-JavaScript/events/218835551/csv/
 // Jun 10 http://www.meetup.com/Ottawa-JavaScript/events/218835536/csv/
+// Jul  8 http://www.meetup.com/Ottawa-JavaScript/events/218835552/csv/
 // then convert tp json
 //    <!-- http://www.convertcsv.com/csv-to-json.htm -->
 
@@ -13,7 +14,7 @@ angular.module("draw", [])
         $scope.helloTo.title = "World, AngularJS";
         $scope.attendees = [{Name:'Daneroo'}];
 
-        $http.get('attendees-2015-05-13.json').then(function(resp) {
+        $http.get('attendees-2015-06-10.json').then(function(resp) {
             console.log(resp);
             $scope.attendees = resp.data;
             // console.log($scope.attendees);
